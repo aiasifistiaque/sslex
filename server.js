28ab1b0098ae5e745864e432bfa6ee4c0600a78e
asifistiaque <asifistiaque.ai@gmail.com>
2021-03-12 02:47:17 +0600

    fixes

--- server.js
+++ server.js
@@ -34,9 +34,4 @@ app.use('/api/products', productsRoute);
 
 const port = process.env.PORT || 5000;
 
-app.listen(
-	port,
-	console.log(
-		`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
-	)
-);
+app.listen(port, console.log(`Server running on port ${port}`.yellow.bold));
