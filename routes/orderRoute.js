3d755936c8295fa5952b1cac9bbb92acd007cd45
asifistiaque <asifistiaque.ai@gmail.com>
2021-03-14 02:13:09 +0600

    asd

--- routes/orderRoute.js
+++ routes/orderRoute.js
@@ -2,10 +2,14 @@ import express from 'express';
 import getSingleOrder from '../controller/order/getSIngleOrder.js';
 import getAllOrders from '../controller/order/getAllOrders.js';
 import addNewOrder from '../controller/order/addNewOrder.js';
+import cors from '../middleware/cors.js';
 
 const router = express.Router();
 
-router.route('./').get(protect, admin, getAllOrders).post(protect, addNewOrder);
+router
+	.route('./')
+	.get(protect, admin, getAllOrders)
+	.post(cors, protect, addNewOrder);
 
 router.get('/:id', protect, getSingleOrder);
 
