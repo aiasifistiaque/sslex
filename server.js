c2dd0c78264cecf5395e277c83d4fd7c0a53ace0
asifistiaque <asifistiaque.ai@gmail.com>
2021-03-14 02:32:42 +0600

    adadsd

--- server.js
+++ server.js
@@ -20,7 +20,12 @@ if (process.env.NODE_ENV === 'development') {
 	app.use(morgan('dev'));
 }
 
-app.use(cors());
+const corsOptions = {
+	origin: 'http://localhost:3000',
+	credentials: true, //access-control-allow-credentials:true
+	optionSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
 
 app.use(function (req, res, next) {
 	res.header('Access-Control-Allow-Origin', '*');
